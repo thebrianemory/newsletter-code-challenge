e4cc442630835b45f1b6b4624e7eb974a9a1e698
thebrianemory <the@brianemory.com>
2017-08-04 10:14:10 -0700

    Add ex_admin dependency
    This will be used to creat an admin dashboard

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule NewsletterCodeChallenge.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:ex_admin, github: "smpallen99/ex_admin"}
     ]
   end
 
