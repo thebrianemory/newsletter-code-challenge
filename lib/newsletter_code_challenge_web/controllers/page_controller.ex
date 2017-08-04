3d34664c244243287f19ae2ad0b26f86195301c5
thebrianemory <the@brianemory.com>
2017-08-04 16:32:12 -0700

    Add redirect if not signed in
    Added an if statement to check if there is a signed in user. If a user
    is not signed in, they will be redirected to the sign up page. If a user
    is signed in, they would be an admin and will be shown the admin
    dashboard. This will require a further check to be added to ensure a
    non-admin isn't signed in and trying to access the admin dashboard.

--- lib/newsletter_code_challenge_web/controllers/page_controller.ex
+++ lib/newsletter_code_challenge_web/controllers/page_controller.ex
@@ -1,7 +1,13 @@
 defmodule NewsletterCodeChallengeWeb.PageController do
   use NewsletterCodeChallengeWeb, :controller
+  alias NewsletterCodeChallenge.Router.Helpers
 
   def index(conn, _params) do
-    render conn, "index.html"
+    if conn.assigns.current_user do
+      render conn, "index.html"
+    else
+      conn
+      |> redirect(to: registration_path(conn, :new))
+    end
   end
 end
