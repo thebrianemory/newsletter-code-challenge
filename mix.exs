992b38c494c5a771417057cd199df81f4c721a05
thebrianemory <the@brianemory.com>
2017-08-04 20:39:35 -0700

    Add swoosh dependency for sending email

--- mix.exs
+++ mix.exs
@@ -41,7 +41,9 @@ defmodule NewsletterCodeChallenge.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:coherence, "~> 0.5"}
+      {:coherence, "~> 0.5"},
+      {:swoosh, "~> 0.9.0"},
+      {:gen_smtp, "~> 0.12.0"}
     ]
   end
 
