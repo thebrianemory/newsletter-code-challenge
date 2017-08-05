d2b8411a5a72368da2ef56268d19d41a9b2da151
thebrianemory <the@brianemory.com>
2017-08-05 09:37:21 -0700

    Add tests for visiting "/"
    Added two test. The first is making sure a visitor gets redirected to
    the sign up page for the newsletter. The second test ensures that a
    signed in admin sees the create a newsletter button.

--- test/newsletter_code_challenge_web/controllers/page_controller_test.exs
+++ test/newsletter_code_challenge_web/controllers/page_controller_test.exs
@@ -1,8 +1,23 @@
 defmodule NewsletterCodeChallengeWeb.PageControllerTest do
   use NewsletterCodeChallengeWeb.ConnCase
+  import NewsletterCodeChallenge.Factory
+  use ExUnit.Case, async: true
 
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  describe "GET /" do
+    test "visitor gets redirected to the newsletteer sign up page", %{conn: conn} do
+      conn = get conn, "/"
+      assert html_response(conn, 302)
+      assert redirected_to(conn) == registration_path(conn, :new)
+    end
+
+    test "signed in admin sees the create newsletter button", %{conn: conn} do
+      admin = insert(:admin)
+
+      conn = conn
+      |> assign(:current_user, admin)
+      |> get("/")
+
+      assert html_response(conn, 200) =~ "Create a newsletter"
+    end
   end
 end
